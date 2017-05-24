746320349d183098c944cce3c1cee8a8afa34362
dhoepp <dhoepp@me.com>
2017-05-24 14:03:07 -0500

    Update button.py

--- button.py
+++ button.py
@@ -1,3 +1,5 @@
+#####
+#script watching for button press
 from scapy.all import *
 from time import sleep
 import requests
@@ -5,10 +7,12 @@ import subprocess
 SLACK_API_URL = 'https://slack.com/api/chat.postMessage'
 
 def arp_display(pkt):
+  #runs a tcpdump on the network looking for the default gateway (this prevents double button pressing)
   if pkt[ARP].pdst == '172.17.0.1':
+    #runs a tcpdump on the network looking for instances of the MAC address of the amazon button
     if pkt[ARP].hwsrc == '44:65:0d:45:fd:9c':
-      print( "1")
-      subprocess.call('/home/pi/button.sh')
-      print("2")
+      #print( "1") #used for testing
+      subprocess.call('/home/pi/button.sh') #initiates button.sh when the button is pressed
+      #print("2") #used for testing
 
 print( sniff(prn=arp_display, filter="arp", store=0))
